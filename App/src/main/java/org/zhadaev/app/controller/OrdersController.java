b1aabc278552ce7d933d68367f3705250a682a63
Alexander Zhadaev <a.v.zhadaev@gmail.com>
2020-09-13 20:27:05 +0400

    completed controller

--- App/src/main/java/org/zhadaev/app/controller/OrdersController.java
+++ App/src/main/java/org/zhadaev/app/controller/OrdersController.java
@@ -1,14 +1,49 @@
 package org.zhadaev.app.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.zhadaev.app.model.Order;
+import org.zhadaev.app.model.Product;
+import org.zhadaev.app.service.IJibxService;
+import org.zhadaev.app.service.IOrderService;
+import org.zhadaev.app.service.IProductService;
+import org.zhadaev.app.service.ISoapClientService;
 
-@Controller
+import java.util.List;
+
+@RestController
 public class OrdersController {
 
-    @GetMapping("/")
-    public String ordersPage() {
-        return "orders";
+    @Autowired
+    private IOrderService orderService;
+
+    @Autowired
+    private IProductService productService;
+
+    @Autowired
+    private IJibxService jibxService;
+
+    @Autowired
+    private ISoapClientService soapClientService;
+
+    @GetMapping("/date")
+    public char[] getDate() {
+        String date = soapClientService.getDate();
+        return date.toCharArray();
+    }
+
+    @GetMapping("/orders")
+    public List<Order> getOrdersList() {
+        return orderService.findAll();
+    }
+
+    @GetMapping("/update")
+    public List<Order> updateProducts() {
+        final String xmlFile = "products.xml";
+        List<Product> products = jibxService.unMarshalProduct(xmlFile);
+        productService.saveList(products);
+        return orderService.findAll();
     }
 
 }
